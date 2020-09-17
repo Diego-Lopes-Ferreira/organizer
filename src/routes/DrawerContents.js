79dd86a81e8ac85ef0037b5d6e2124dc20ee34e6
Diego-Lopes-Ferreira <diego.lopes.ferreira.br@gmail.com>
2020-09-17 18:33:32 -0300

    feat(drawer): implemented switcher

--- src/routes/DrawerContents.js
+++ src/routes/DrawerContents.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { View, Text, Switch } from 'react-native';
-import { DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
-import { FontAwesome } from '@expo/vector-icons';
-import { useTheme } from '../contexts/Theme';
+import React from "react";
+import { View, Text, Switch } from "react-native";
+import { DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
+import { FontAwesome } from "@expo/vector-icons";
+import { useTheme } from "../contexts/Theme";
 
 const screensInfo = [
   {
-    name: 'One',
-    label: 'The Label',
-    icon: 'home',
+    name: "One",
+    label: "The Label",
+    icon: "home",
   },
   {
-    name: 'Two',
-    label: 'The Label 2',
-    icon: 'coffee',
+    name: "Two",
+    label: "The Label 2",
+    icon: "coffee",
   },
   {
-    name: 'Tree',
-    label: 'The Label 3',
-    icon: 'gift',
+    name: "Tree",
+    label: "The Label 3",
+    icon: "gift",
   },
 ];
 
@@ -27,36 +27,57 @@ function DrawerContents(props) {
   return (
     <View style={{ flex: 1 }}>
       <DrawerContentScrollView {...props}>
-        <View>
-          <Text>App Title</Text>
+        <View style={theme.styles.drawer.drawerAppName}>
+          <Text style={theme.styles.drawer.drawerItemTitle}>App Title</Text>
         </View>
         {screensInfo.map((item) => (
           <DrawerItem
-            icon={({ color, size }) => <FontAwesome name={item.icon} color={color} size={size} />}
+            icon={({ color, size }) => (
+              <FontAwesome name={item.icon} color={color} size={size} />
+            )}
             label={item.label}
             onPress={() => props.navigation.jumpTo(item.name)}
             key={screensInfo.indexOf(item)}
           />
         ))}
       </DrawerContentScrollView>
-      <View>
-        <Switch
-          onValueChange={() => {
-            setTheme(theme.name === 'dark' ? 'light' : 'dark');
-            props.navigation.closeDrawer();
-          }}
-          value={theme.name === 'dark'}
-        />
-      </View>
+      <ThemeSwitcherItem
+        theme={theme}
+        setTheme={setTheme}
+        close={props.navigation.closeDrawer}
+      />
       <View>
         <DrawerItem
           icon={({ color, size }) => (
             <FontAwesome name="sign-out" color={color} size={size} />
           )}
           label="Sign Out"
-          onPress={() => global.console.log('youSignedOut')}
+          onPress={() => global.console.log("youSignedOut")}
         />
       </View>
+    </View>
+  );
+}
+
+function ThemeSwitcherItem({ theme, setTheme, close }) {
+  return (
+    <View style={theme.styles.drawer.drawerItemView}>
+      <Text style={theme.styles.drawer.drawerItemText}>Dark Mode</Text>
+      <Switch
+        //background
+        trackColor={{ false: theme.colors.gray1, true: theme.colors.gray1 }}
+        //ball
+        thumbColor={
+          theme.name === "dark" ? theme.colors.primary2 : theme.colors.primary2
+        }
+        //idk
+        ios_backgroundColor={theme.colors.gray1}
+        onValueChange={() => {
+          setTheme(theme.name === "dark" ? "light" : "dark");
+          close(); //props.navigation.closeDrawer();
+        }}
+        value={theme.name === "dark"}
+      />
     </View>
   );
 }
