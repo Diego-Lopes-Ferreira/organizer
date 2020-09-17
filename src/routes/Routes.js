0f630f596d789a9147bdbf52c45cc5566f5a8f42
Diego-Lopes-Ferreira <diego.lopes.ferreira.br@gmail.com>
2020-09-17 18:02:26 -0300

    fix(routes): unused imports

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,11 +1,10 @@
 /*
   Routes
 */
-import React from 'react';
+import React from "react";
 
-import AuthRoutes from './AuthRoutes';
-import DefaultRoutes from './DefaultRoutes';
-import StackNavigator from './StackNavigator';
+import DefaultRoutes from "./DefaultRoutes";
+//import StackNavigator from './StackNavigator';
 
 export default function AppRoutes() {
   return <DefaultRoutes />;
