d684d8dd3db8be5a288f5664495d1d13ba2d6607
Diego-Lopes-Ferreira <diego.lopes.ferreira.br@gmail.com>
2020-12-08 09:21:40 -0300

    install dependencies

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={ styles.container }>
+      <Text style={ styles.text }>Hello, world!!!</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -14,8 +14,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0f0f0f',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#f0f0f0',
+  }
 });
